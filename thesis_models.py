aaff6fc8df8be3ca9a58d070ae751e2b223c53d7
PangBing <39650126+panbinwot@users.noreply.github.com>
2019-09-21 22:25:13 -0400

    update helper file

---
+++ thesis_models.py
@@ -0,0 +1,5 @@
+'''
+Author: Bin Pan
+Date: 09/21/2018
+Comment: this is the helper function for the thesis project. The module includes several models and defines errors. I wrote it in the functional way to make it easier.
+'''
