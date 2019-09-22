b693eac0b2c12b77565decf0074cf777d114fd42
PangBing <39650126+panbinwot@users.noreply.github.com>
2019-09-22 00:06:23 -0400

    something

--- thesis_models.py
+++ thesis_models.py
@@ -3,3 +3,14 @@ Author: Bin Pan
 Date: 09/21/2018
 Comment: this is the helper function for the thesis project. The module includes several models and defines errors. I wrote it in the functional way to make it easier.
 '''
+import numpy as np 
+
+def  r_square(y_pred, y_test):
+    y_pred, y_test = np.array(y_pred), np.array(y_test)
+    res = ((y_pred - y_test)^2).sum() / len(y_test)
+    print(res)
+    return res
+
+a = [1,2,4,5]
+b = [2,3,4,5]
+r_square(a,b)
